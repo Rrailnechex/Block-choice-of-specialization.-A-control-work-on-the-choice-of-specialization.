ce0d15e20d7e2e6742259f5232e2416b67dc5af6
Ygor <wanerosu8@gmail.com .com>
2022-11-25 20:12:07 +0300

    решаю проблему добавления в новый массив пустых ст

--- TheProgram/Program.cs
+++ TheProgram/Program.cs
@@ -12,6 +12,7 @@
 использовать массивы
  */
 
+
 void Print1DArray(string[] inputArray)
 {
     Console.Write("[");
@@ -23,42 +24,38 @@ void Print1DArray(string[] inputArray)
 }
 
 
-
-
-
-
-string[] firstAr = { "1234", "1567", "computer science", "-2", "e" };
+string[] firstAr = { "1234", "1567", "computer science", "-2", "eьжлропав", "0", "][" };
+string[] midAr = new string[firstAr.GetLength(0)];
 int newL = 0;
 int maxL = 2;
 
+
+//узнаём длинну нового массива и выбираем элементы
 for (int i = 0; i < firstAr.GetLength(0); i++)
 {
-    Console.WriteLine($"{i} -> цикл");
+    Console.WriteLine($"for = {i}");
     if (firstAr[i].Length <= maxL)
     {
         newL++;
-        Console.WriteLine($"{newL} -> newL newL newL newL");
-        Console.WriteLine($"{firstAr[i].Length} -> firstAr[i].Length");
+        midAr[i] = firstAr[i];
+        Console.WriteLine($"midAr[i] = {midAr[i]}; i = {i}");
     }
 }
+Console.WriteLine($"длинна узнана = {newL}");
 
-Console.WriteLine($"{newL} -> ------------------- дальше");
 
+//добавляем в новый массив строки
 string[] newAr = new string[newL];
 
 for (int i = 0; i < newAr.GetLength(0); i++)
 {
     if (firstAr[i].Length <= maxL)
     {
-        newL++;
-        Console.WriteLine($"{newL} -> newL newL newL newL");
-        Console.WriteLine($"{firstAr[i].Length} -> firstAr[i].Length");
         newAr[i] = firstAr[i];
-
+        Console.WriteLine($"цикл добавления = {i}");
     }
 }
 
-Console.WriteLine($" -> ------------------- конец");
 
 Console.Write($"Изначальный массив = ");
 Print1DArray(firstAr);
